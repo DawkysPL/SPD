f8ab490e85e0cc13818e3965b8517e833b1cc9ab
Michal <michal_rogala@op.pl>
2019-03-05 20:25:07 +0100

    code clean

--- Zad1/SPD1/SPD1/Program.cs
+++ Zad1/SPD1/SPD1/Program.cs
@@ -7,9 +7,8 @@ namespace SPD1
         static void Main(string[] args)
         {
             string[] text = System.IO.File.ReadAllLines(@"test.txt"); // SPD1 -> BIN -> DEBUG
-            string daneStartowe = text[0].TrimStart(' ');
+            string daneStartowe = text[0];
             WczytajLiczbeZadanMaszyn(daneStartowe);
-            //Console.WriteLine($"To jest liczba zadan: {Maszyna.liczbaZadan}, a to maszyny: {Maszyna.liczbaMaszyn}");
 
             // tworzenie maszyn
             List<Maszyna> Maszyny = new List<Maszyna>();
@@ -28,56 +27,22 @@ namespace SPD1
             string laczymy = a.ToString() + b.ToString();
             return laczymy;
         }
+
         static void WczytajLiczbeZadanMaszyn(string daneStartowe)
         {
-            if (daneStartowe[1].ToString() == " ")
-                Maszyna.liczbaZadan = Convert.ToInt32(daneStartowe[0].ToString()); //DODAC OBSLUGE WIEKSZEJ LICZBY ZADAN I MASZYN NARAZIE DLA POJEDYNCZYCH CYFR
-            else
-            {
-                if (daneStartowe[2].ToString() == " ")
-                {
-                    Maszyna.liczbaZadan = Convert.ToInt32(LaczStringi(
-                        Convert.ToInt32(daneStartowe[0].ToString()),
-                        Convert.ToInt32(daneStartowe[1].ToString())));
-                }
-                else
-                {
-                    string dwaZlaczone = LaczStringi(
-                        Convert.ToInt32(daneStartowe[0].ToString()),
-                        Convert.ToInt32(daneStartowe[1].ToString()));
-                    Maszyna.liczbaZadan = Convert.ToInt32(dwaZlaczone + daneStartowe[2]);
-                }
-            }
-
-            if (daneStartowe[daneStartowe.Length - 2].ToString() == " ")
-                Maszyna.liczbaMaszyn = Convert.ToInt32(daneStartowe[daneStartowe.Length - 1].ToString());
-            else
-            {
-                Maszyna.liczbaMaszyn = Convert.ToInt32(LaczStringi(
-                    Convert.ToInt32(daneStartowe[daneStartowe.Length - 2].ToString()),
-                    Convert.ToInt32(daneStartowe[daneStartowe.Length - 1].ToString())));
-            }
+            string[] result = daneStartowe.Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
+            Maszyna.liczbaZadan = Convert.ToInt32(result[0]);
+            Maszyna.liczbaMaszyn = Convert.ToInt32(result[1]);
         }
         static void WczytajDaneDoMaszyn(List<Maszyna> Maszyny, string[] text)
         {
-            string NewString;
-            for (int i = 1; i < text.Length; i++)
+            string[] result;
+            for (int i = 1; i <= Maszyna.liczbaZadan; i++)
             {
-                NewString = text[i];
+                result = text[i].Split(new char[] {' '},StringSplitOptions.RemoveEmptyEntries);
                 foreach (var maszyna in Maszyny)
                 {
-                    NewString = NewString.TrimStart(' ');
-                    int czas = -1;
-                    if (NewString[1] != ' ')
-                    {
-                        czas = Convert.ToInt32(NewString[0].ToString() + NewString[1].ToString());
-                    }
-                    else
-                    {
-                        czas = Convert.ToInt32(NewString[0].ToString());
-                    }
-                    maszyna.TimeWithTask.Add(new TimeWithTask(i, czas));
-                    NewString = NewString.Remove(0, 2); 
+                    maszyna.TimeWithTask.Add(new TimeWithTask(i,Convert.ToInt32(result[maszyna.Id-1])));
                 }
             }
         }
--- Zad1/SPD1/SPD1/TimeWithTask.cs
+++ Zad1/SPD1/SPD1/TimeWithTask.cs
@@ -12,7 +12,7 @@
             NumerTaska = _nrTaska;
             Time = _time;
         }
-        public int NumerTaska { get; set; }
-        public int Time { get; set; }
+        public int NumerTaska { get; private set; }
+        public int Time { get; private set; }
     }
 }
