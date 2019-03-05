a00d827d7339dbfc7f6d72acf19aa4b18ac86f69
Michal <michal_rogala@op.pl>
2019-03-05 10:05:29 +0100

    Przygotowanie programu do wczytywania liczby zadan,maszyn oraz danych do maszyn

---
+++ Zad1/SPD1/SPD1/TimeWithTask.cs
@@ -0,0 +1,18 @@
+﻿namespace SPD1
+{
+    class TimeWithTask
+    {
+        public TimeWithTask()
+        {
+
+        }
+
+        public TimeWithTask(int _nrTaska, int _time)
+        {
+            NumerTaska = _nrTaska;
+            Time = _time;
+        }
+        public int NumerTaska { get; set; }
+        public int Time { get; set; }
+    }
+}
